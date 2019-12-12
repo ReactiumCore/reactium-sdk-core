08551ff8bac1408074302edea7767185112df86f
John Dillick <john@dillick.us>
2019-12-12 12:42:23 -0500

    Imports ReactTestUtils from proper location. See https://reactjs.org/docs/test-utils.html

--- test/component.spec.js
+++ test/component.spec.js
@@ -1,46 +1,70 @@
 import { render } from './enzyme';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import utils from 'react-dom/cjs/react-dom-test-utils.development';
-import { SDK, RegisteredComponent, ComponentToRegister } from '../test-components';
+import ReactTestUtils from 'react-dom/test-utils';
+import {
+    SDK,
+    RegisteredComponent,
+    ComponentToRegister,
+} from '../test-components';
 
 test('useHookComponent() - component not registered', async () => {
-    const notRegistered = document.createElement("div");
-    ReactDOM.render(<>
-        <RegisteredComponent />
-    </>, notRegistered);
+    const notRegistered = document.createElement('div');
+    ReactDOM.render(
+        <>
+            <RegisteredComponent />
+        </>,
+        notRegistered,
+    );
     expect(notRegistered.innerHTML).toEqual('DefaultComponent');
     ReactDOM.unmountComponentAtNode(notRegistered);
-})
+});
 
 test('useHookComponent() - component registered', async () => {
-    const registered = document.createElement("div");
-    const uuid = await SDK.Component.register('registered-component', ComponentToRegister)
-    await utils.act(async () => {
-        ReactDOM.render(<>
-            <RegisteredComponent />
-            </>, registered);
+    const registered = document.createElement('div');
+    const uuid = await SDK.Component.register(
+        'registered-component',
+        ComponentToRegister,
+    );
+    await ReactTestUtils.act(async () => {
+        ReactDOM.render(
+            <>
+                <RegisteredComponent />
+            </>,
+            registered,
+        );
     });
 
     expect(registered.innerHTML).toEqual('RegisteredComponent');
     ReactDOM.unmountComponentAtNode(registered);
     await SDK.Hook.unregister(uuid);
-})
+});
 
 test('useHookComponent() - capabilities denied', async () => {
-    const registered = document.createElement("div");
-    const uuid = await SDK.Hook.register('capability-check', async (caps, strict, context) => {
-        context.permitted = false;
-    });
+    const registered = document.createElement('div');
+    const uuid = await SDK.Hook.register(
+        'capability-check',
+        async (caps, strict, context) => {
+            context.permitted = false;
+        },
+    );
 
-    await utils.act(async () => {
-        await SDK.Component.register('registered-component', ComponentToRegister, 0, ['some.cap'])
-        ReactDOM.render(<>
-            <RegisteredComponent />
-            </>, registered);
+    await ReactTestUtils.act(async () => {
+        await SDK.Component.register(
+            'registered-component',
+            ComponentToRegister,
+            0,
+            ['some.cap'],
+        );
+        ReactDOM.render(
+            <>
+                <RegisteredComponent />
+            </>,
+            registered,
+        );
     });
 
     expect(registered.innerHTML).toEqual('DefaultComponent');
     ReactDOM.unmountComponentAtNode(registered);
     await SDK.Hook.unregister(uuid);
-})
+});
