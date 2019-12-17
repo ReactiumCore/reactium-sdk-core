c4b50abb9a31588b5b6deea4999e7dec48cf6ecd
John Dillick <john@dillick.us>
2019-12-17 12:00:46 -0500

    Move doc to docs

---
+++ docs/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "@atomic-reactor/reactium-sdk-core",  "version": "0.0.11",  "description": "Reactium SDK Core",  "title": "Reactium SDK Core",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-12-17T17:00:24.850Z",    "url": "http://apidocjs.com",    "version": "0.19.0"  }});
