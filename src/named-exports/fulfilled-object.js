b5781f193fefd794637d3d879b92099c4bfffe3e
Cam Tullos <cam@tullos.ninja>
2020-02-25 18:10:18 -0500

    Added useFulFilledObject hook.

---
+++ src/named-exports/fulfilled-object.js
@@ -0,0 +1,77 @@
+/**
+ * @api {ReactHook} useFulFilledObject(object,keys) useFulFilledObject()
+ * @apiName useFulFilledObject
+ * @apiGroup ReactHook
+ * @apiDescription Asyncronous React hook that determines if the supplied object
+ has values for the supplied keys. Useful when you have many `useEffect` calls
+ and need to know if multiple pieces of the state are set and ready for rendering.
+ * @apiParam {Object} The object to check.
+ * @apiParam {Array} List of object paths to validate.
+ * @apiExample Example Usage:
+
+import React, { useEffect, useState } from 'react';
+import { useFulFilledObject } from 'reactium-core/sdk';
+
+const MyComponent = () => {
+
+    const [state, setNewState] = useState({});
+    const [updatedState, ready, attempts] = useFulFilledObject(state, ['msg', 'timestamp']);
+
+    const setState = newState => {
+        newState = { ...state, ...newState };
+        setNewState(newState);
+    };
+
+    useEffect(() => {
+        if (!state.msg) {
+            setState({ msg: 'ok'});
+        }
+    }, [state]);
+
+    useEffect(() => {
+        if (!state.timestamp) {
+            setState({ timestamp: Date.now() });
+        }
+    }, [state]);
+
+    const render = () => {
+        return ready !== true ? null : <div>I'm READY!!</div>;
+    };
+
+    return render();
+};
+ */
+ export const useFulFilledObject = (obj = {}, keys = []) => {
+     const [ready, setReady] = useState(false);
+     const count = useRef(0);
+     const ival = useRef();
+
+     const clear = () =>  {
+         clearInterval(ival.current);
+         return () => {};
+     };
+
+     const validate = () =>
+         new Promise(resolve => {
+             clear();
+             ival.current = setInterval(() => {
+                 count.current += 1;
+                 const completed = keys.filter(key => !!op.get(obj, key));
+                 if (completed.length !== keys.length) return;
+                 clear();
+                 resolve(true);
+             }, 1);
+         });
+
+     useAsyncEffect(
+         async mounted => {
+             if (ready === true) return clear();
+             const results = await validate();
+             if (mounted()) setReady(results);
+             return clear();
+         },
+         [obj, keys, count.current],
+     );
+
+     return [obj, ready, count.current];
+ };
--- src/named-exports/index.js
+++ src/named-exports/index.js
@@ -2,6 +2,7 @@ export * from './component';
 export * from './derived-state';
 export * from './event-handle';
 export * from './focus-effect';
+export * from './fulfilled-object';
 export * from './handle';
 export * from './is-container';
 export * from './redux';
