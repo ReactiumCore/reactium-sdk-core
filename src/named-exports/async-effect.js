5b63b2fbc7417513891ea7c313e37e32c321f8a3
John Dillick <john@dillick.us>
2020-03-10 15:23:21 -0400

    Sllight improvement to useAsyncEffect. Add counter example for useAsyncEffect for possible misuse.  0.0.57

--- src/named-exports/async-effect.js
+++ src/named-exports/async-effect.js
@@ -53,17 +53,41 @@ const MyComponent = props => {
 };
 * @apiExample StandAlone Import
 import { useAsyncEffect } from '@atomic-reactor/reactium-sdk-core';
+* @apiExample Wrong Usage
+import React, { useState } from 'react';
+import { useAsyncEffect } from 'reactium-core/sdk';
+
+const MyComponent = props => {
+    const [show, setShow] = useState(false);
+
+    // change state allowing value to show
+    // asynchrounously, but only if component is still mounted
+    useAsyncEffect(async isMounted => {
+        // Warning: don't do this, wait until promise resolves to check isMounted()!!
+        if (isMounted()) { // this may be true *before* promise resolves and false *after*
+            setShow(false);
+            await new Promise(resolve => setTimeout(resolve, 3000));
+            setShow(true);
+        }
+
+        // unmount callback
+        return () => {};
+    }, [ props.value ]);
+
+    return (
+        {show && <div>{props.value}</div>}
+    );
+};
  */
 export const useAsyncEffect = (cb, deps) => {
-    const updater = new AsyncUpdate();
-
-    const doEffect = async () => {
+    const doEffect = async updater => {
         return cb(updater.isMounted);
     }
 
     useEffect(() => {
+        const updater = new AsyncUpdate();
         updater.mounted = true;
-        const effectPromise = doEffect();
+        const effectPromise = doEffect(updater);
 
         return () => {
             updater.mounted = false;
@@ -74,5 +98,4 @@ export const useAsyncEffect = (cb, deps) => {
             });
         };
     }, deps);
-
 };
