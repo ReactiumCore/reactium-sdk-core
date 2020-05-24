eee2ccf7a2bf807d27c5be545ea232a13de6d005
Cam Tullos <cam@tullos.ninja>
2020-05-24 14:10:31 -0400

    Added registry test

---
+++ test/registry.spec.js
@@ -0,0 +1,41 @@
+import SDK from '../lib';
+import { expect } from 'chai';
+
+describe('useIsContainer', () => {
+    const { Registry } = SDK;
+
+    const Hotkeys = new Registry('Hotkeys', 'id');
+
+    it('register', () => {
+        Hotkeys.register('media-save', {
+            callback: () => {},
+            key: 'mod+s',
+            order: 0,
+            //scope: document,
+        });
+
+        Hotkeys.register('content-save', {
+            callback: () => {},
+            key: 'mod+s',
+            order: 0,
+            //scope: document,
+        });
+
+        Hotkeys.register('shortkey-save', {
+            callback: () => {},
+            key: 'mod+s',
+            order: 0,
+            //scope: document,
+        });
+
+        expect(Hotkeys.list).to.have.lengthOf(3);
+    });
+
+    it ('unregister', () => {
+        Hotkeys.unregister('media-save');
+        Hotkeys.unregister('content-save');
+        Hotkeys.unregister('shortkey-save');
+
+        expect(Hotkeys.list).to.have.lengthOf(0);
+    });
+});
