9b386e1fb0df07fa6ef41ac908e097052c075aa4
John Dillick <john@dillick.us>
2020-07-09 17:06:32 -0400

    Add Server.AppHeaders registry for registering application headers for server.

--- src/server/index.js
+++ src/server/index.js
@@ -10,6 +10,8 @@ const Server = {};
  */
 Server.Middleware = Utils.registryFactory('ExpressMiddleware', 'name', Utils.Registry.MODES.CLEAN);
 
+Server.AppHeaders = Utils.registryFactory('AppHeaders', 'name', Utils.Registry.MODES.CLEAN);
+
 Server.AppScripts = Utils.registryFactory('AppScripts', 'name', Utils.Registry.MODES.CLEAN);
 
 Server.AppSnippets = Utils.registryFactory('AppSnippets', 'name', Utils.Registry.MODES.CLEAN);
