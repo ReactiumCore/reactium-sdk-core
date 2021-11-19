bdab9599aaa1eda9a5a774d603f4d74b2d54f308
John Dillick <john@dillick.us>
2021-11-19 10:00:05 -0500

    @atomic-reactor/reactium-sdk-core@1.2.9
    Fix sync state updater loss in useSyncHandle.

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,6 +1,6 @@
 define({
   "name": "@atomic-reactor/reactium-sdk-core",
-  "version": "1.2.8",
+  "version": "1.2.9",
   "description": "Reactium SDK Core",
   "title": "Reactium SDK Core",
   "sampleUrl": false,
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-11-11T02:19:38.609Z",
+    "time": "2021-11-19T14:57:41.022Z",
     "url": "http://apidocjs.com",
     "version": "0.24.0"
   }
