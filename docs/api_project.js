a3ea8eb8efb70887b1ae0e9c400a3a53acc4d92f
John Dillick <john@dillick.us>
2022-05-22 01:02:20 -0400

    @atomic-reactor/reactium-sdk-core@1.2.15 - Attempt to fix malformed previous publish of sdk-core which breaks arcli.

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,6 +1,6 @@
 define({
   "name": "@atomic-reactor/reactium-sdk-core",
-  "version": "1.2.13",
+  "version": "1.2.15",
   "description": "Reactium SDK Core",
   "title": "Reactium SDK Core",
   "sampleUrl": false,
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-11-19T19:46:06.890Z",
+    "time": "2022-05-22T05:00:23.548Z",
     "url": "http://apidocjs.com",
     "version": "0.24.0"
   }
